da8ad6a73c070a2f6be8e9f477f8698c947ad2b5
Sadiyya Nailarani <Naila.Nailarani@warwick.ac.uk>
2025-11-10 22:43:18 +0000

    Set up cleaning method for 1 frame

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 
 cofid_pickle_path = "food_dataset/cofid_pickle"
 cofid_excel_path = "food_dataset/cofid.xlsx"
-sheets = ["1.2 Factors", "1.3 Proximates", "1.4 Inorganics", "1.5 Vitamins"]
+sheets = ["1.3 Proximates", "1.4 Inorganics", "1.5 Vitamins"]
 
 # If excel sheet not cached - cache it, else load into cofid_dfs
 if not os.path.exists(cofid_pickle_path):
@@ -14,18 +14,64 @@ if not os.path.exists(cofid_pickle_path):
 
 cofid_dfs = pd.read_pickle(cofid_pickle_path)
 
-factors_df = cofid_dfs[sheets[0]]
-proximates_df = cofid_dfs[sheets[1]]
-inorganics_df = cofid_dfs[sheets[2]]
-vitamins_df = cofid_dfs[sheets[3]]
+# Loading the relevant sheets into dataframes
+proximates_df = cofid_dfs[sheets[0]]
+inorganics_df = cofid_dfs[sheets[1]]
+vitamins_df = cofid_dfs[sheets[2]]
 
-print(factors_df.head(), "\n")
-
-
-# for name, df in cofid_dfs.items():
-#     print(f"--- {name} ---")
-#     print(df.head(), "\n")
+# print(proximates_df.columns, "\n")
 
 
+# Cleaning time
+# trace value represented with Tr
+# N - nutrient is present in significant quantities but no reliable information on the amount
+"""
+https://realpython.com/python-data-cleaning-numpy-pandas/
+1. Drop unnecessary columns
+2. Change index if needed
+3. Tidy fields
+4. Clean Columns
+5. 
 
+Replacing trace values:
+- Will employ these rules for handling trace values:
+-- Macronutrients: Tr -> 0.1g/0.1kcal
+-- Vitamins: Tr -> 0.1mg
+-- Minerals: Tr -> 0.01mg
 
+"""
+
+macros = ["Energy (kcal) (kcal)", "Energy (kJ) (kJ)", "Protein (g)", "Fat (g)", "Carbohydrate (g)", "Water (g)", "Total sugars (g)"]
+vitamins = []
+minerals = []
+
+# Cleaning proximates_df
+# We only need columns: Food Code, Food Name, Group, Water (g), Protein (g), Fat (g), Carbohydrate (g), Energy (kcal) (kcal), Energy (kJ) (kJ)
+
+proximates_columns = ["Food Code", "Food Name", "Group", "Water (g)", "Protein (g)", "Fat (g)", "Carbohydrate (g)", "Energy (kcal) (kcal)", "Energy (kJ) (kJ)", "Total sugars (g)"]
+proximate_measures = proximates_columns[3:]
+print(proximate_measures)
+proximates_df = proximates_df[proximates_columns]
+
+# Replace "N" with NaN and drop rows with NaN
+proximates_df = proximates_df.replace("N", np.nan)
+proximates_df = proximates_df.dropna()
+
+# Cleaning fields
+# Replace values then convert all columns to integers
+for col in proximates_df[proximate_measures]:
+    if col in macros or col in vitamins:
+        proximates_df[col] = proximates_df[col].replace("Tr", 0.1)
+    elif col in minerals:
+        proximates_df[col] = proximates_df[col].replace("Tr", 0.01)
+
+print(proximates_df.iloc[:10, :15])
+print()
+
+# Once cleaned fields, dropped rows and stuff - re-index rows
+proximates_df = proximates_df.reset_index(drop= True)
+print(proximates_df.iloc[:10, :15])
+
+# Convert all columns into numbers or smth like that
+proximates_df[proximate_measures] = proximates_df[proximate_measures].apply(pd.to_numeric)
+print(proximates_df.dtypes)
