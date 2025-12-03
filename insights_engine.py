66604c18dccb212203173048ff0b9a38286b1619
Sadiyya Nailarani <Naila.Nailarani@warwick.ac.uk>
2025-12-03 22:22:30 +0000

    Refactored nutritition summation code

--- insights_engine.py
+++ insights_engine.py
@@ -21,50 +21,35 @@ def calc_nutrition_range(uid, date_start, date_end):
     # List of all food logs
     food_logs = session.scalars(get_food_stmt).all()
 
-    # Getting all nutrients from the proximates table
-    proximates = [col.key for col in(Proximates.__table__.c[1:])]
-    proximates_sums = {p:0 for p in proximates}
-
-    # For each proximate, find the total value 
-    for log in food_logs:
-        print(log.food_code)
-        food = log.food
-        food_prox = food.proximates
-        for p in proximates:
-            proximates_sums[p] += log.quantity * getattr(food_prox, p)
-        
-
-    # Getting all nutrients from the inorganics table
-    inorganics = [col.key for col in(Inorganics.__table__.c[1:])]
-    inorganics_sums = {i:0 for i in inorganics}
-
-    # For each inorganics, find the total value 
-    for log in food_logs:
-        food = log.food
-        food_inorg = food.inorganics
-        for i in inorganics:
-            inorganics_sums[i] += log.quantity * getattr(food_inorg, i)
-
-
-    # Getting all nutrients from the vitamins table
-    vitamins = [col.key for col in(Vitamins.__table__.c[1:])]
-    vitamins_sums = {v:0 for v in vitamins}
-
-    # For each vitamins, find the total value 
-    for log in food_logs:
-        food = log.food
-        food_vits = food.vitamins
-        for v in vitamins:
-            vitamins_sums[v] += log.quantity * getattr(food_vits, v)
+    proximates_sums = calculate_nutrient_sums(Proximates, food_logs, "proximates")
+    inorganics_sums = calculate_nutrient_sums(Inorganics, food_logs, "inorganics")
+    vitamins_sums = calculate_nutrient_sums(Vitamins, food_logs, "vitamins")
 
 
     print(proximates_sums)
     print(inorganics_sums)
     print(vitamins_sums)
 
-             
 
-calc_nutrition_range(2, date(2025, 1, 1), date(2025, 12, 31))
+
+# Calculates nutrient sums for a particular table (proximates, inorganics, vitamins)
+def calculate_nutrient_sums(table, food_logs, table_name):
+    # Getting all nutrients from the proximates table
+    nutrient_cols = [col.key for col in(table.__table__.c[1:])]
+    nutrient_sums = {n:0 for n in nutrient_cols}
+
+    # For each proximate, find the total value 
+    for log in food_logs:
+        food = log.food
+        food_nutrients = getattr(food, table_name)
+        for n in nutrient_cols:
+            nutrient_sums[n] += log.quantity * getattr(food_nutrients, n)
+
+    return nutrient_sums
+
+
+
+calc_nutrition_range(18, date(2025, 1, 1), date(2025, 12, 31))
 
 def calc_daily_nutrition():
     pass
